a6a8352e5306b0baa89731cf55c41d62ffba3f72
Bob van den Hoogen <bob@getstrm.com>
2023-12-20 17:50:51 +0100

    chore: add welcome message

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -14,6 +14,8 @@ const EnvPrefix = "PACE"
 const DefaultConfigFilename = "config"
 const DefaultConfigFileSuffix = ".yaml"
 
+const DefaultLastSeenFilename = "pace-cli-last-seen"
+
 var DefaultConfigFileContents = []byte(`# The following configuration options are reflected in the CLI's flags
 # api-host: localhost:50051
 # output: yaml
