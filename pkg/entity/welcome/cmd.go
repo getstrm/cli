9c1c0ff9db37eb97c5aa1a59916f3a311a100e05
Robin Trietsch <robin@getstrm.com>
2023-12-21 20:50:56 +0100

    chore(pace-92): do not use integer value in string interpolation

--- pkg/entity/welcome/cmd.go
+++ pkg/entity/welcome/cmd.go
@@ -17,9 +17,10 @@ func DisableCmd() *cobra.Command {
 			lastSeenCommandFilepath := path.Join(common.ConfigPath(), common.DefaultLastSeenFilename)
 			os.WriteFile(
 				lastSeenCommandFilepath,
-				[]byte(fmt.Sprintf("%d", 9999999999)),
+				[]byte("9999999999"),
 				0644,
 			)
+			fmt.Println("Welcome message disabled")
 		},
 		Args: cobra.ExactArgs(0),
 	}
